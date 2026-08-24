748ad59403f68096e7892b6e7010fa3d9efc8176
Badal Thapa <KhBadalYa@gmail.com>
2026-08-24 23:29:30 +0545

    Fix: Add rest_framework_simplejwt.token_blacklist to INSTALLED_APPS in settings.py and execute python manage.py migrate

--- backend/linguabridge_backend/settings.py
+++ backend/linguabridge_backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "channels",
     "accounts",
     "chat",
